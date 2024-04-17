1f1ed60f694dfcb3d9c6bf72a7a98a4e129ee9f6
IsWladi <isaacx3984@outlook.com>
2024-04-17 15:51:36 -0400

    refactor: mueve endpoint obtener/items a crud items

--- api/app/routers/crud_categorias.py
+++ api/app/routers/crud_categorias.py
@@ -19,65 +19,6 @@ dependencia_bdd = Annotated[MongoClient, Depends(obtener_bdd)] # para usar: bdd:
 
 router = APIRouter(prefix="/api/categorias", tags=["CRUD de categorias"])
 
-@router.get("/obtener/items", status_code=200)
-async def obtener_items_coleccion(usuario_conectado: dependencia_autenticacion, bdd: dependencia_bdd, categoria: str = "all"):
-    """
-    Obtiene todos los ítems de una categoría específica o la colección completa de ítems para el usuario autenticado.
-
-    Parámetros:
-    - categoria (str, opcional): Especifica la categoría de ítems a retornar. Por defecto es "all", que indica que se deben retornar todas las categorías.
-
-    Encabezados requeridos:
-    - Authorization (str): Token de acceso del tipo "Bearer" requerido para autenticar la solicitud. Debe ser incluido en el encabezado de la solicitud como "Authorization: Bearer \<token\>".
-
-    Respuestas:
-    - 200: Retorna un objeto JSON con los ítems de la categoria especificada  o en su defecto, todos los ítems de todas las categorías.
-    - 401: No autorizado - Autenticación fallida o no proporcionada. Se devuelve cuando el usuario no está autenticado o si el token de autenticación es inválido.
-    - 400: Solicitud incorrecta - La categoría especificada no existe en la colección del usuario, asegurarse de ingresar una categoría válida.
-    - 404: No encontrado - No se encontraron colecciones para el usuario conectado, probablemente porque el usuario se registró recientemente y no ha creado ninguna colección.
-
-    Ejemplos de uso:
-    ```
-    GET /api/categorias/obtener/items?categoria=libros
-    Authorization: Bearer <token>
-    ```
-
-    Devuelve los ítems de la categoría "libros" para el usuario autenticado.
-
-    ```
-    GET /api/categorias/obtener/items
-    Authorization: Bearer <token>
-    ```
-
-    Devuelve todos los ítems de todas las categorias para el usuario autenticado.
-    """
-
-    coleccion_usuarios = bdd["usuarios"]
-    coleccion_colecciones = bdd["colecciones"]
-
-    #obtener el id de las colecciones del usuario conectado
-    respuesta = coleccion_usuarios.find_one({"username": usuario_conectado.username}, {"coleccion": 1})
-
-    try:
-        id_colecciones = respuesta['coleccion'].id
-    except:
-        raise HTTPException(status_code=404, detail="No se encontraron colecciones para el usuario conectado")
-
-    if categoria == "all":
-        todas_las_categorias = coleccion_colecciones.find_one({"_id": ObjectId(id_colecciones)}, {"_id": 0})
-        return convertir_id_a_str(todas_las_categorias)
-
-    else: # retornar solo la categoria especificada
-        categoria_especifica = coleccion_colecciones.find_one({"_id": ObjectId(id_colecciones)},
-                                                              {f"{categoria}": 1, "_id": 0})
-        categoria_especifica = convertir_id_a_str(categoria_especifica)
-
-        if f"{categoria}" in categoria_especifica:
-            return categoria_especifica
-        else:
-            raise HTTPException(status_code=400, detail=f"La categoría {categoria} no existe en la colección, ingresar una categoría válida.")
-
-
 # endpoint para agregar una nueva categoría a la colección del usuario (vacía, sin ítems)
 @router.post("/agregar/categoria/{nombre_categoria}")
 async def agregar_categoria(nombre_categoria: str, usuario_conectado: dependencia_autenticacion, bdd: dependencia_bdd):
