b377c326654fc3eda468db8df92956df99ce88b8
IsWladi <isaacx3984@outlook.com>
2024-04-23 17:03:50 -0400

    feat: genera esqueleto vista generar tarjeta

--- lizard/src/app/tarjeta/tarjeta.page.ts
+++ lizard/src/app/tarjeta/tarjeta.page.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjeta.page.scss'],
 })
 export class TarjetaPage implements OnInit {
+  // configuración de la tarjeta (color y fuente)
+  colorTarjeta: string = '';
+  fuenteTarjeta: string = '';
+  // datos del usuario
+  urlFoto: string = '';
+  username: string = '';
+  ultimoTitulo: string = '';
+  // cantidad de items por tipo de colección
+  cantLibros: number = 0;
+  cantFiguras: number = 0;
+  cantVideojuegos: number = 0;
+  cantReplicaArmas: number = 0;
+  cantCartas: number = 0;
+
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  generarTarjeta(){
+
+  }
+
+
 
 }
