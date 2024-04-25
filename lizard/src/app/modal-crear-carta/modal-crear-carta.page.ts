4be028cb8c2085c680b78d6275db3ba6fe315758
IsWladi <isaacx3984@outlook.com>
2024-04-25 12:19:50 -0400

    feat: estructura modal crear carta y videojuego listo

--- lizard/src/app/modal-crear-carta/modal-crear-carta.page.ts
+++ lizard/src/app/modal-crear-carta/modal-crear-carta.page.ts
@@ -1,13 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import {ICartaCreate} from '../coleccionInterfaces';
 
 @Component({
   selector: 'app-modal-crear-carta',
   templateUrl: './modal-crear-carta.page.html',
   styleUrls: ['./modal-crear-carta.page.scss'],
 })
+
 export class ModalCrearCartaPage implements OnInit {
 
-  constructor() { }
+  // objeto que contendrá los datos de la nueva carta a crear
+  carta: ICartaCreate = {
+    // datos comunes
+    fecha_adquisicion: "",
+    url_foto: "",
+    nombre: "",
+    tipo: "",
+    descripcion: "",
+    // datos específicos de carta
+    juego: "",
+  }
+
+  constructor(private modalController: ModalController) { }
+
+
+  cerrarModal() {
+    this.modalController.dismiss();
+  }
 
   ngOnInit() {
   }
--- lizard/src/app/modal-crear-videojuego/modal-crear-videojuego.page.ts
+++ lizard/src/app/modal-crear-videojuego/modal-crear-videojuego.page.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import {IVideojuegoCreate} from '../coleccionInterfaces';
 
 @Component({
   selector: 'app-modal-crear-videojuego',
   templateUrl: './modal-crear-videojuego.page.html',
   styleUrls: ['./modal-crear-videojuego.page.scss'],
 })
+
 export class ModalCrearVideojuegoPage implements OnInit {
 
-  constructor() { }
+  // objeto que contendrá los datos del nuevo videojuego a crear
+  videojuego: IVideojuegoCreate = {
+    // datos comunes
+    fecha_adquisicion: "",
+    url_foto: "",
+    nombre: "",
+    tipo: "",
+    descripcion: "",
+    // datos específicos de un videojuego
+    plataforma: "",
+    genero: "",
+    creador: "",
+    caracteristicas: "",
+  }
+
+  constructor(private modalController: ModalController) { }
+
+
+  cerrarModal() {
+    this.modalController.dismiss();
+  }
 
   ngOnInit() {
   }
