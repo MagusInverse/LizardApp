21fb34a86c358b41da38589902653eadd58fb682
IsWladi <isaacx3984@outlook.com>
2024-04-25 12:04:15 -0400

    feat: estructura de modal crear arma listo

--- lizard/src/app/modal-crear-arma/modal-crear-arma.page.ts
+++ lizard/src/app/modal-crear-arma/modal-crear-arma.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import {IArmaCreate} from '../coleccionInterfaces';
 
 @Component({
   selector: 'app-modal-crear-arma',
   templateUrl: './modal-crear-arma.page.html',
   styleUrls: ['./modal-crear-arma.page.scss'],
 })
+
 export class ModalCrearArmaPage implements OnInit {
 
-  constructor() { }
+  // objeto que contendrá los datos de la nueva arma a crear
+  arma: IArmaCreate = {
+    // datos comunes
+    fecha_adquisicion: "",
+    url_foto: "",
+    nombre: "",
+    tipo: "",
+    descripcion: "",
+    // datos específicos de arma
+    material: "",
+    tamano: 0,
+    peso: 0,
+    fabricante: "",
+}
+
+  constructor(private modalController: ModalController) { }
+
+
+  cerrarModal() {
+    this.modalController.dismiss();
+  }
 
   ngOnInit() {
   }
-
 }
+
