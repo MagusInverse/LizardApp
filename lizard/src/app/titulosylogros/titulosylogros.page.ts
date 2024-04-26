ea4703869cebb2be68ba995fbe9c71e5f27a1810
IsWladi <isaacx3984@outlook.com>
2024-04-25 20:20:03 -0400

    feat: variables para almacenar titulos y logros

--- lizard/src/app/titulosylogros/titulosylogros.page.ts
+++ lizard/src/app/titulosylogros/titulosylogros.page.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Titulo {
+  nombre: string;
+  fecha: string;
+}
+
+interface Logro {
+  nombre: string;
+  fecha: string;
+}
+
 @Component({
   selector: 'app-titulosylogros',
   templateUrl: './titulosylogros.page.html',
   styleUrls: ['./titulosylogros.page.scss'],
 })
 export class TitulosylogrosPage implements OnInit {
+
+  titulos: Titulo[] = [] // array para guardar los títulos del usuario
+  logros: Logro[] = [] // array para guardar los logros del usuario
 
   constructor() { }
 
