5c04bd9176e461f1e34ac40d255a6b070ad72f1d
IsWladi <isaacx3984@outlook.com>
2024-04-27 10:52:35 -0400

    feat: items carta, figura, juego; agrega pattern arma

--- lizard/src/app/itemcarta/itemcarta.page.ts
+++ lizard/src/app/itemcarta/itemcarta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ICarta} from '../coleccionInterfaces';
 
 @Component({
   selector: 'app-itemcarta',
@@ -7,7 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemcartaPage implements OnInit {
 
+  carta: ICarta = {
+    _id: "",
+    // datos comunes
+    fecha_adquisicion: "",
+    url_foto: "",
+    nombre: "",
+    tipo: "",
+    descripcion: "",
+    // datos específicos de carta
+    juego: "",
+  }
+
   constructor() { }
+
+  cambiarEstadoEditable(idElemento: string) {
+    let elemento = document.getElementById(idElemento);
+    if (elemento) { // Solo continua si elemento no es null.
+      // si tiene el atributo readonly, lo elimina
+      if (elemento.hasAttribute("readonly")) {
+        elemento.removeAttribute("readonly");
+        elemento.focus();
+      }
+      // si no lo tiene, se lo pone
+      else {
+        elemento.setAttribute("readonly", "true");
+      }
+    }
+  }
+
+  actualizarItem(){
+    console.log("Actualizar item basado en el _id del item");
+  }
+
+
+  eliminarItem(){
+    console.log("Eliminar item basado en el _id del item");
+  }
 
   ngOnInit() {
   }
--- lizard/src/app/itemfigura/itemfigura.page.ts
+++ lizard/src/app/itemfigura/itemfigura.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IFigura} from '../coleccionInterfaces';
 
 @Component({
   selector: 'app-itemfigura',
@@ -7,8 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemfiguraPage implements OnInit {
 
+  figura: IFigura = {
+    _id: "",
+    // datos comunes
+    fecha_adquisicion: "",
+    url_foto: "",
+    nombre: "",
+    tipo: "",
+    descripcion: "",
+    // datos específicos de figura
+    origen: "",
+    tamano: 0,
+    material: "",
+    empresa: "",
+  }
+
   constructor() { }
 
+  cambiarEstadoEditable(idElemento: string) {
+    let elemento = document.getElementById(idElemento);
+    if (elemento) { // Solo continua si elemento no es null.
+      // si tiene el atributo readonly, lo elimina
+      if (elemento.hasAttribute("readonly")) {
+        elemento.removeAttribute("readonly");
+        elemento.focus();
+      }
+      // si no lo tiene, se lo pone
+      else {
+        elemento.setAttribute("readonly", "true");
+      }
+    }
+  }
+
+  actualizarItem(){
+    console.log("Actualizar item basado en el _id del item");
+  }
+
+
+  eliminarItem(){
+    console.log("Eliminar item basado en el _id del item");
+  }
   ngOnInit() {
   }
 
